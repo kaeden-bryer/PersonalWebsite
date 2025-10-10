65b5b19f59014593796f33ee492ed72cde46fd50
HououinKyouma555 <130073365+HououinKyouma555@users.noreply.github.com>
2025-10-09 22:46:16 -0400

    Create glass effect

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,5 @@
-
+import React from 'react'
+import '../index.css'
 
 function Contact() {
     return (
@@ -7,7 +8,14 @@ function Contact() {
                 id="contact"
                 className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-b from-blue-100 via-blue-300 to-blue-200"
             >
-                <h1 className="flex justify-center text-[4em]">Contact Me</h1>
+                <img src="pink-cube.png" alt="pink cube" className="absolute right-[10px]"></img>
+
+                <div className="glass-container w-[80%] h-full flex flex-col justify-center items-center">
+                    <h1 className="flex justify-center text-[4em]">Contact Me</h1>
+                    <p className="flex justify-center text-[2em]">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime minus provident totam unde, quis iure incidunt et praesentium illum optio omnis ipsa soluta repellat voluptas debitis dignissimos. Nulla, aperiam quaerat.
+                    </p>
+                </div>
             </section>
         </>
     )
